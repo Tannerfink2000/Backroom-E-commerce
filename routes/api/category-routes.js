8bb15d3a8cd2365687714b38db72748ebadf11b8
Tanner Fink <tannerfink2000@gmail.com>
2023-08-01 20:06:18 -0400

    did cat routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,59 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// Get all categories
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({ include: [Product] });
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// Get category by id
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findOne({ 
+      where: { id: req.params.id }, 
+      include: [Product]
+    });
+    res.json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// Create new category
+router.post('/', async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// Update category by id
+router.put('/:id', async (req, res) => {
+  try {
+    const category = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// Delete category by id
+router.delete('/:id', async (req, res) => {
+  try {
+    const category = await Category.destroy({ where: { id: req.params.id } });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;

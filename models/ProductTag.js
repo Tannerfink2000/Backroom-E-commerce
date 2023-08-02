1a582ef1b3dab2be7d10f917b2fdea423b5b605c
Tanner Fink <tannerfink2000@gmail.com>
2023-08-01 20:04:57 -0400

    finished  models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,43 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
+// Define the structure of the ProductTag model
+const productTagStructure = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: 'id',
+    },
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id',
+    },
+  },
+};
+
+// Define additional settings for the ProductTag model
+const productTagSettings = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product_tag',
+};
+
+// Initialize ProductTag model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+// Set up fields and rules for ProductTag model
+ProductTag.init(productTagStructure, productTagSettings);
 
 module.exports = ProductTag;
--- models/Tag.js
+++ models/Tag.js
@@ -1,20 +1,32 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
+// Define the structure of the Tag model
+const tagStructure = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  tag_name: {
+    type: DataTypes.STRING,
+  },
+};
+
+// Define additional settings for the Tag model
+const tagSettings = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'tag',
+};
+
+// Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
-Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
-);
+// Set up fields and rules for Tag model
+Tag.init(tagStructure, tagSettings);
 
 module.exports = Tag;
